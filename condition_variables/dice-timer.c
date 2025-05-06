3c9ea19978184f0e3e12a0080392b5623c2087c0
happy7656 <happy518596@gmail.com>
2025-05-07 02:11:37 +0900

    condition_variables changed

--- condition_variables/dice-timer.c
+++ condition_variables/dice-timer.c
@@ -8,7 +8,7 @@
 
 int num = 0;
 int generated = 0;
-//pthread_mutex_t mutex_num = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_num = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mutex_gen = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond_gen = PTHREAD_COND_INITIALIZER;
 
@@ -18,11 +18,11 @@ void *child(void *arg)
     srand(time(NULL));
     for (int i = 0; i < NLOOPS; ++i) {
 	pthread_mutex_lock(&mutex_gen);
-//	pthread_mutex_lock(&mutex_num);
+	pthread_mutex_lock(&mutex_num);
 	num = (rand() % 6) + 1;
 	generated = 1;
 	pthread_cond_signal(&cond_gen);
-//	pthread_mutex_unlock(&mutex_num);
+	pthread_mutex_unlock(&mutex_num);
 	pthread_mutex_unlock(&mutex_gen);
 	sleep(1);
     }
@@ -41,13 +41,13 @@ int main()
 	pthread_mutex_lock(&mutex_gen);
 	while (!generated)
 	    pthread_cond_wait(&cond_gen, &mutex_gen);
-//	pthread_mutex_lock(&mutex_num);
+	pthread_mutex_lock(&mutex_num);
 	printf("parent: %d\n", num);
 	generated = 0;
-//	pthread_mutex_unlock(&mutex_num);
+	pthread_mutex_unlock(&mutex_num);
 	pthread_mutex_unlock(&mutex_gen);
     }
-//    pthread_join(t1, NULL);
+    pthread_join(t1, NULL);
     printf("parent: return\n");
     return 0;
 }
